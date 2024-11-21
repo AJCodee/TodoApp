8605f7cec7a4f8feb9cee62d2e027440fdcf1680
Alex Hedges <AJHedges@hotmail.com>
2024-11-21 20:46:31 +0000

    Added database.py

--- database.py
+++ database.py
@@ -1,1 +1,13 @@
 # This is going to be the database for the TodoApp
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+
+enigne = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=enigne)
+
+Base = declarative_base()
+
