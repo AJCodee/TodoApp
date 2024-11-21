d5c66b5e9001622ab307fe6d014905d577614ab0
Alex Hedges <AJHedges@hotmail.com>
2024-11-21 20:58:32 +0000

    Added a models file

---
+++ models.py
@@ -0,0 +1,13 @@
+# Models for the tables that will show in Database.
+
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey 
+
+class Todos(Base):
+    __tabelname__ = 'Todos'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
