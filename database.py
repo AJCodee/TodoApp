dac6def8823a8306293f6dccae6b01385dadea27
Alex Hedges <AJHedges@hotmail.com>
2024-11-22 20:20:29 +0000

    Created db injection / first endpoint

--- database.py
+++ database.py
@@ -1,4 +1,6 @@
 # This is going to be the database for the TodoApp
+# To use the SQLite3 database in terminal use - sqlite3 todos.db
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
 import models
-from database import enigne
+from models import Todos
+from database import enigne, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=enigne)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+db_dependecy = Annotated[Session, Depends(get_db)]
+        
+@app.get("/")
+async def read_all(db: db_dependecy):
+    return db.query(Todos).all()
