a256a1613065c41825d37f540c24981655bc5d9a
Alex Hedges <AJHedges@hotmail.com>
2024-11-24 15:41:39 +0000

    Added User table and Hashing Passwords

--- routers/auth.py
+++ routers/auth.py
@@ -1,7 +1,33 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from models import Users
+from passlib.context import CryptContext # type: ignore
 
 router = APIRouter()
 
-@router.get("/auth/")
-async def get_user():
-    return {'user': 'authenticated'}
+# Code for hashing a user password for security.
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+class CreateUserRequest(BaseModel):
+    """Request model for creating a new user."""
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+@router.post("/auth/")
+async def create_user(create_user_request: CreateUserRequest):
+    """Create a new user."""
+    create_user_model = Users(
+        email = create_user_request.email,
+        username = create_user_request.username,
+        first_name = create_user_request.first_name,
+        last_name = create_user_request.last_name,
+        role = create_user_request.role,
+        hashed_password = bcrypt_context.hash(create_user_request.password),
+        is_active = True
+    )
+    
+    return create_user_model
