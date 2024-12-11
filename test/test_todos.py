c9969246dbd7ee4f4400c437f38f84fb57b85da2
Alex Hedges <AJHedges@hotmail.com>
2024-12-11 20:00:13 +0000

    Started creating dependency for testing

---
+++ test/test_todos.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.pool import StaticPool
+from database import Base, sessionmaker
+from main import app
+from routers.todos import get_db
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass = StaticPool,
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+app.dependency_override[get_db] = override_get_db
