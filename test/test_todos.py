2d6f7dbf0a53119df6570020fa45b2b8f29d605d
Alex Hedges <AJHedges@hotmail.com>
2024-12-12 20:31:46 +0000

    Created a testdb for testing

--- test/test_todos.py
+++ test/test_todos.py
@@ -3,6 +3,9 @@ from sqlalchemy.pool import StaticPool
 from database import Base, sessionmaker
 from main import app
 from routers.todos import get_db
+from routers.auth import get_current_user
+from fastapi.testclient import TestClient
+from fastapi import status
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
 
@@ -23,7 +26,18 @@ def override_get_db():
     finally:
         db.close()
         
-app.dependency_override[get_db] = override_get_db
+def override_get_current_user():
+    return {"username": "codewithalextest", "id": 1, "user_role": "admin"}
+        
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = override_get_current_user
+
+client = TestClient(app)
+
+def test_read_all_authenticated():
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == []
 
 # Havent needed to add .. before any of my imports as it has been finding the files. 
-# However, If i start getting import issues then I need to refactor to do ..main instead of just main.
+# However, If i start getting import issues then I need to refactor to do ..main instead of just main for etc.
