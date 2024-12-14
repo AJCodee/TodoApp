ee53872684859c5b0979483cce19f06f76891f16
Alex Hedges <AJHedges@hotmail.com>
2024-12-14 20:47:43 +0000

    Created more test for Todos

--- test/test_todos.py
+++ test/test_todos.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.pool import StaticPool
 from database import Base, sessionmaker
 from main import app
@@ -6,6 +6,8 @@ from routers.todos import get_db
 from routers.auth import get_current_user
 from fastapi.testclient import TestClient
 from fastapi import status
+import pytest
+from models import Todos
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
 
@@ -34,10 +36,61 @@ app.dependency_overrides[get_current_user] = override_get_current_user
 
 client = TestClient(app)
 
-def test_read_all_authenticated():
+@pytest.fixture
+def test_todo():
+    todo = Todos(
+        title="Learn to code!",
+        description="Need to learn everyday",
+        priority=5,
+        complete=False,
+        owner_id=1
+    )
+    
+    db = TestingSessionLocal()
+    db.add(todo)
+    db.commit()
+    yield todo
+    with engine.connect() as connection:
+        connection.execute(text("DELETE FROM todos;"))
+        connection.commit()
+
+def test_read_all_authenticated(test_todo):
     response = client.get("/")
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == []
+    assert response.json() == [{'title': 'Learn to code!', 
+                                'description': 'Need to learn everyday', 'id': 1, 
+                                'priority': 5, 'owner_id': 1, 'complete': False}]
+    
+def test_read_one_authenticated(test_todo):
+    response = client.get("/todo/1")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {'title': 'Learn to code!', 
+                                'description': 'Need to learn everyday', 'id': 1, 
+                                'priority': 5, 'owner_id': 1, 'complete': False}
+    
+def test_read_one_authenticated_not_found():
+    response = client.get("/todo/999")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {'detail': 'Todo not found'}
+    
+def test_create_todo(test_todo):
+    request_data = {
+        'title': 'New todo!',
+        'description': 'New todo description',
+        'priority': 5,
+        'complete': False
+    }
+    
+    response = client.post('/todo/', json=request_data)
+    assert response.status_code == 201
+    
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 2).first()
+    assert model is not None # Ensure the model is found
+    assert model.title == request_data.get('title')
+    assert model.description == request_data.get('description')
+    assert model.priority == request_data.get('priority')
+    assert model.complete == request_data.get('complete')
 
 # Havent needed to add .. before any of my imports as it has been finding the files. 
 # However, If i start getting import issues then I need to refactor to do ..main instead of just main for etc.
