cfbc88254f4601f26b7ffaa7600066e41934beaa
Alex Hedges <AJHedges@hotmail.com>
2024-12-17 19:41:41 +0000

    Created and added tests for users

--- test/utils.py
+++ test/utils.py
@@ -4,7 +4,8 @@ from database import Base, sessionmaker
 from main import app
 from fastapi.testclient import TestClient
 import pytest
-from models import Todos
+from models import Todos, Users
+from routers.auth import bcrypt_context
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
 
@@ -46,4 +47,24 @@ def test_todo():
     yield todo
     with engine.connect() as connection:
         connection.execute(text("DELETE FROM todos;"))
+        connection.commit()
+        
+@pytest.fixture
+def test_user():
+    user = Users(
+        username="codewithalextest",
+        email="codewithalextest@email.com",
+        first_name="Alex",
+        last_name="Hedges",
+        hashed_password=bcrypt_context.hash("testpassword"),
+        role="admin",
+        phone_number="(111)-111-1111"
+    )
+    
+    db = TestingSessionLocal()
+    db.add(user)
+    db.commit()
+    yield user
+    with engine.connect() as connection:
+        connection.execute(text("DELETE FROM users;"))
         connection.commit()
