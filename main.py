c9a07347d12f5762c89777594cb0c7e3e7c1e241
Alex Hedges <AJHedges@hotmail.com>
2024-12-23 13:32:32 +0000

    Started work on Html files

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import models
 from database import engine
 from routers import auth, todos, admin, users
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="TodoApp/templates")
+
+@app.get("/")
+def test(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
 @app.get("/healthy")
 async def health_check():
