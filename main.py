0228bc2963ec74e0385bbd54b1707a0e5f989fa8
Alex Hedges <AJHedges@hotmail.com>
2024-12-26 16:39:22 +0000

    Added a directory for static/css/js

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import models
 from database import engine
 from routers import auth, todos, admin, users
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def test(request: Request):
