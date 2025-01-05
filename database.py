589d9acb8784d9beb9997b4bf8c2a20d31035152
Alex Hedges <AJHedges@hotmail.com>
2025-01-05 13:52:21 +0000

    Trying out Aiven database

--- database.py
+++ database.py
@@ -5,15 +5,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # This line is used to connect to the postgresql / pgadmin database.
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:passpost@localhost/TodoApplicationDatabase'
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+#SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:passpost@localhost/TodoApplicationDatabase'
+#engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 
 #Code for making the database connect to aiven (Working on)
 # Path to the downloaded CA certificate from Aiven
-#SSL_CERT_PATH = '/absolute/path/to/ca.pem'
-#SQLALCHEMY_DATABASE_URL = 'postgresql://avnadmin:AVNS_TOEMvgcDE3O-Iu08Zqm@pg-3f4ef746-deployment-database.g.aivencloud.com:10732/defaultdb?sslmode=require'
-#engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"sslmode": "require", "sslrootcert": SSL_CERT_PATH})
+SSL_CERT_PATH = '/absolute/path/to/ca.pem'
+SQLALCHEMY_DATABASE_URL = 'postgresql://avnadmin:AVNS_TOEMvgcDE3O-Iu08Zqm@pg-3f4ef746-deployment-database.g.aivencloud.com:10732/defaultdb?sslmode=require'
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"sslmode": "require", "sslrootcert": SSL_CERT_PATH})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
